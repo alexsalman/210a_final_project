ad39c934dd4d9d46ccd7d6a0710479480083c27d
Alex Salman <aalsalma@ucsc.edu>
2021-03-11 20:42:10 -0800

    changed rust file structure

--- target/debug/deps/hello_rust-4ef388a11ad56df2.d
+++ target/debug/deps/hello_rust-4ef388a11ad56df2.d
@@ -1,8 +1,8 @@
-/Users/alexsalman/hello-rust/target/debug/deps/hello_rust-4ef388a11ad56df2.rmeta: src/lib.rs src/block.rs src/hashable.rs
+/Users/alexsalman/hello-rust/210a_final_project/target/debug/deps/hello_rust-4ef388a11ad56df2.rmeta: src/lib.rs src/block.rs src/hashable.rs
 
-/Users/alexsalman/hello-rust/target/debug/deps/libhello_rust-4ef388a11ad56df2.rlib: src/lib.rs src/block.rs src/hashable.rs
+/Users/alexsalman/hello-rust/210a_final_project/target/debug/deps/libhello_rust-4ef388a11ad56df2.rlib: src/lib.rs src/block.rs src/hashable.rs
 
-/Users/alexsalman/hello-rust/target/debug/deps/hello_rust-4ef388a11ad56df2.d: src/lib.rs src/block.rs src/hashable.rs
+/Users/alexsalman/hello-rust/210a_final_project/target/debug/deps/hello_rust-4ef388a11ad56df2.d: src/lib.rs src/block.rs src/hashable.rs
 
 src/lib.rs:
 src/block.rs:
--- target/debug/deps/hello_rust.d
+++ target/debug/deps/hello_rust.d
@@ -1,5 +1,5 @@
-/Users/alexsalman/hello-rust/target/debug/deps/hello_rust: src/main.rs
+/Users/alexsalman/hello-rust/210a_final_project/target/debug/deps/hello_rust: src/main.rs
 
-/Users/alexsalman/hello-rust/target/debug/deps/hello_rust.d: src/main.rs
+/Users/alexsalman/hello-rust/210a_final_project/target/debug/deps/hello_rust.d: src/main.rs
 
 src/main.rs:
